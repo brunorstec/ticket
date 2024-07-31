a68f85eb3c68ec2d7100a229b9c40f80ae99235c
brunorstec <brunorstec@gmail.com>
2024-07-31 19:17:03 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 mkdir data
 mkdir static
 mkdir templates
-mkdir /ticket/static/images
-mv admin.html dashboard.html index.html thank_you.html /ticket/templates
-mv styles.css /ticket/static/
-pip install Flask pandas matplotlib
+mkdir /static/images
+mv admin.html dashboard.html index.html thank_you.html /templates
+mv styles.css /static/
